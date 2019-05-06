3fa2fe1f3ce78edda9058685713de4752a51a1a7
bhumicagupta <44159154+bhumicagupta@users.noreply.github.com>
2019-05-06 06:37:23 +0000

    Update Fortify.sh

--- Fortify.sh
+++ Fortify.sh
@@ -1,4 +1,4 @@
-var=`curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{ 'settingsName': 'Default', 'overrides': {'scanName': 'JFrog-scan5','startUrls': ['https://repo.test.cloud.thdm.local'], 'crawlAuditMode':'CrawlAndAudit','startOption': 'url','dontStartScan': false,'scanScope': 'unrestricted'} }" http://10.210.54.246:8083/webinspect/scanner/scans`
+var=`curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{ 'settingsName': 'Default', 'overrides': {'scanName': 'JFrog-scan5','startUrls': ['http://10.210.54.246:8083/webinspect/swagger/ui/index'], 'crawlAuditMode':'CrawlAndAudit','startOption': 'url','dontStartScan': false,'scanScope': 'unrestricted'} }" http://10.210.54.246:8083/webinspect/scanner/scans`
 
 echo $var
 echo "ScanID $var"
